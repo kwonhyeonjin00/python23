5cda51ec0f81447351b8b31f4e3cb5b49704b3ae
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-16 13:20:43 +0900

    Title: 수열의 순서 바꾸기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241016/수열의 순서 바꾸기/reorder-sequence.py
+++ 241016/수열의 순서 바꾸기/reorder-sequence.py
@@ -10,18 +10,36 @@ arr = list(map(int, input().split()))
 chk_num = max(arr)
 
 cnt = 0
+x = 0
 
 while True:
+    c = 0
+    t = []
     if check(arr):
         print(cnt)
         break
 
-    t = arr.pop(0)
-    if t == chk_num:
-        print(cnt + 1)
-        break
+    while True:
+        if c == 1:
+            break 
 
-    idx = arr.index(min(arr))
+        if arr[0] == x + 1:
+            t.append(arr.pop(0))
+            x = t[-1]
+        else:
+            c = 1
 
-    arr.insert(idx, t)
-    cnt += 1
+    if t[-1] == chk_num:
+        cnt += len(t)
+        for i in range(len(t)):
+            arr.append(t.pop(0))
+
+    else:
+        cnt += len(t)
+        idx = arr.index(min(arr))
+
+        for i in range(len(t)):
+            arr.insert(idx, t.pop())
+    print(arr)
+
+print(cnt)
