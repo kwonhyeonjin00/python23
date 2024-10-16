92b7d29952c79e82924a2ee75193fcf5f0956e34
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-16 13:47:41 +0900

    Title: 수열의 순서 바꾸기, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/수열의 순서 바꾸기/reorder-sequence.py
+++ 241016/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,48 +1,16 @@
-def check(arr):
-    for i in range(n):
-        if arr[i] != i - 1:
-            return False
-    return True
-
 n = int(input())
 arr = list(map(int, input().split()))
 
-chk_num = max(arr)
-
 cnt = 0
 
-while True:
-    c = 0
-    t = []
-    if check(arr):
-        break
+x = arr.pop()
+chk = 0
 
-    t.append(arr.pop(0))
-
-    while True:
-        if c == 1:
-            break 
-        if arr:
-            if t[-1] + 1 == arr[0]:
-                t.append(arr.pop(0))
-
-            else:
-                c = 1
-        else:
-            c = 1
-
-    if len(t) == n:
-        break
-    elif t[-1] == chk_num:
-        cnt += len(t)
-        for i in range(len(t)):
-            arr.append(t.pop(0))
-
+for i in range(n - 1):
+    t = arr.pop()
+    if t + 1 == x:
+        chk += 1
+        x = t
     else:
-        cnt += len(t)
-        idx = arr.index(t[-1] + 1)
-
-        for i in range(len(t)):
-            arr.insert(idx, t.pop())
-
-print(cnt)
+        print(n - chk)
+        break
