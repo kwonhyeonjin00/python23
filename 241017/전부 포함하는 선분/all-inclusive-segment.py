6b673a6ba08046751a73768f4e61917df7eeb5fe
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-17 03:12:33 +0900

    Title: 전부 포함하는 선분, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241017/전부 포함하는 선분/all-inclusive-segment.py
+++ 241017/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,34 +1,19 @@
 n = int(input())
+arr0 = []
+arr1 = []
 
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-min_x = arr[0][0]
-max_x = arr[0][1]
-min_idx = 0
-max_idx = 0
 for i in range(n):
-    if arr[i][0] < min_x:
-        min_idx = i
-        min_x = arr[i][0]
-    if arr[i][1] > max_x:
-        max_idx = i
-        max_x = arr[i][1]
+    a, b = map(int, input().split())
+    arr0.append(a)
+    arr1.append(b)
 
-a = arr[min_idx][1] - arr[min_idx][0]
-b = arr[max_idx][1] - arr[max_idx][0]
+max_x1 = arr1.pop(arr1.index(max(arr1)))
+max_x2 = arr1.pop(arr1.index(max(arr1)))
 
-if a >= b:
-    arr.pop(min_idx)
+min_x1 = arr0.pop(arr0.index(min(arr0)))
+min_x2 = arr0.pop(arr0.index(min(arr0)))
+
+if max_x1 - max_x2 >= min_x2 - min_x1:
+    print(max_x2 - min_x1)
 else:
-    arr.pop(max_idx)
-
-min_x = arr[0][0]
-max_x = arr[0][1]
-
-for i in range(n - 1):
-    if arr[i][0] < min_x:
-        min_x = arr[i][0]
-    if arr[i][1] > max_x:
-        max_x = arr[i][1]
-
-print(max_x - min_x)
+    print(max_x1 - min_x2)
