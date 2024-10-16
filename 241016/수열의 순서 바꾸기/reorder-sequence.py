0ca73fa1832cafdb0654a6cba351e7b04a148a4d
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-16 12:12:59 +0900

    Title: 수열의 순서 바꾸기, Time: 5013ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 241016/수열의 순서 바꾸기/reorder-sequence.py
+++ 241016/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,4 +1,27 @@
+def check(arr):
+    for i in range(n):
+        if arr[i] != i - 1:
+            return False
+    return True
+
 n = int(input())
 arr = list(map(int, input().split()))
 
-print(arr.index(max(arr)) + 1)
+chk_num = max(arr)
+
+cnt = 0
+
+while True:
+    if check(arr):
+        print(cnt)
+        break
+
+    t = arr.pop(0)
+    if t == chk_num:
+        print(cnt + 1)
+        break
+
+    idx = arr.index(min(arr))
+
+    arr.insert(idx, t)
+    cnt += 1
