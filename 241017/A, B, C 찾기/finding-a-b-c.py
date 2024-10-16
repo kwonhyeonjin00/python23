85c348c9538695aff3d597e70572846ae8113c6c
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-17 02:23:58 +0900

    Title: A, B, C 찾기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/A, B, C 찾기/finding-a-b-c.py
+++ 241017/A, B, C 찾기/finding-a-b-c.py
@@ -2,18 +2,9 @@ arr = list(map(int, input().split()))
 
 A = arr.pop(arr.index(min(arr)))
 
-ABC = arr.pop(arr.index(max(arr)))
+B = min(arr)
 
-BC = ABC - A
-
-for i in range(len(arr) - 1):
-    for j in range(i + 1, len(arr)):
-        if arr[i] + arr[j] == BC:
-            if arr[i] <= arr[j]:
-                B = arr[i]
-                C = arr[j]
-            else:
-                B = arr[j]
-                C = arr[i]
+ABC = max(arr)
+C = ABC - (A + B)
 
 print(A, B, C)
