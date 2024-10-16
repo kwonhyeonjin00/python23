9ee9fac1dacda04d82aee2526db8633f1afc904b
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-17 03:01:41 +0900

    Title: 전부 포함하는 선분, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241017/전부 포함하는 선분/all-inclusive-segment.py
+++ 241017/전부 포함하는 선분/all-inclusive-segment.py
@@ -2,13 +2,33 @@ n = int(input())
 
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-arr.sort()
+min_x = arr[0][0]
+max_x = arr[0][1]
+min_idx = 0
+max_idx = 0
+for i in range(n):
+    if arr[i][0] < min_x:
+        min_idx = i
+        min_x = arr[i][0]
+    if arr[i][1] > max_x:
+        max_idx = i
+        max_x = arr[i][1]
 
-a = arr[0][1] - arr[0][0]
-b = arr[n - 1][1] - arr[n - 1][0]
+a = arr[min_idx][1] - arr[min_idx][0]
+b = arr[max_idx][1] - arr[max_idx][0]
 
 if a >= b:
-    print(arr[n - 1][1] - arr[1][0])
-
+    arr.pop(min_idx)
 else:
-    print(arr[-2][1] - arr[0][0])
+    arr.pop(max_idx)
+
+min_x = arr[0][0]
+max_x = arr[0][1]
+
+for i in range(n - 1):
+    if arr[i][0] < min_x:
+        min_x = arr[i][0]
+    if arr[i][1] > max_x:
+        max_x = arr[i][1]
+
+print(max_x - min_x)
