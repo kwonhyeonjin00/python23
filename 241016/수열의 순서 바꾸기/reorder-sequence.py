204beec3cd9e5d405c04e9c801029d6428854012
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-16 14:16:30 +0900

    Title: 수열의 순서 바꾸기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/수열의 순서 바꾸기/reorder-sequence.py
+++ 241016/수열의 순서 바꾸기/reorder-sequence.py
@@ -5,7 +5,7 @@ cnt = 0
 
 x = arr.pop()
 chk = 1
-
+t = 0
 for i in range(n - 1):
     #print(arr[n - 2 - i])
     if arr[n - 2 - i] + 1 < x:
@@ -13,6 +13,8 @@ for i in range(n - 1):
         x = arr[n - 2 - i]
     else:
         print(n - chk)
+        t = 1
         break
 
-print(0)
+if t == 0:
+    print(0)
